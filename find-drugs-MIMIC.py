9bf4f59a23cd8b764dfbc4523ca9bd1fef60c3f0
Tom Pollard <tpollard@mit.edu>
2017-11-17 17:15:08 -0500

    python3; run on db

--- find-drugs-MIMIC.py
+++ find-drugs-MIMIC.py
@@ -3,6 +3,8 @@
 #--------------------------------
 # Written by Marzyeh Ghassemi, CSAIL, MIT 
 # Sept 21, 2012
+# Updated for Python 3, added Notebook, db connection
+# by Tom J. Pollard 13 Nov, 2017
 # Please contact the author with errors found. 
 # mghassem {AT} mit {DOT} edu
 #--------------------------------
@@ -15,23 +17,6 @@ import re
 import string
 import sys
 import time
-
-# PATH TO DIRECTORY WITH NOTES
-NOTES_PATH = "dir/Path/Needed"
-
-# LIMIT FOR PARSING
-NUM_FILES_LIMIT = None
-
-# PATH TO FILES WITH DRUGS
-#   Files should have a line for each distinct drug type, 
-#   and drugs shld be separated by a vertical bar '|'
-DRUGS_LIST_PATH = "dir/Path/Needed"
-SSRI_FILE = os.path.join(DRUGS_LIST_PATH, "SSRI_list.txt")
-MISC_FILE = os.path.join(DRUGS_LIST_PATH, "MISC_list.txt")
-
-# OUTPUT
-SUMMARY_FILE = "dir/output.csv"
-
 
 def addToDrugs(line, drugs, listing, genList):
     """
@@ -46,9 +31,9 @@ def addToDrugs(line, drugs, listing, genList):
     #   the found key to the generics list
     """
     genList = dict(enumerate(genList))
-    genList = dict((v,k) for k, v in genList.iteritems())
+    genList = dict((v,k) for k, v in genList.items())
 
-    for (generic, names) in listing.iteritems():
+    for (generic, names) in listing.items():
         if re.search(names, line, re.I):
             drugs[genList[generic]] = 1
     return drugs
@@ -70,10 +55,29 @@ def readDrugs(f, genList):
     genList.append(generics)
     return dict(zip(generics, lines))
 
-def main():
-    # Print the variables being used for inputs
-    print "Using %s notes from %s" % (NUM_FILES_LIMIT or "ALL", NOTES_PATH)
-    print "Using drugs from %s" % (DRUGS_LIST_PATH)
+def search(NOTES, 
+           SSRI_FILE = os.path.join(os.getcwd(), "SSRI_list.txt"), 
+           MISC_FILE = os.path.join(os.getcwd(), "MISC_list.txt"),
+           SUMMARY_FILE = "output.csv",
+           VERBOSE = False):
+    """
+    ###### Search the notes
+    # NOTES: dataframe loaded from the noteevents table
+    # SSRI_FILE: list of SSRI drugs to search for
+    # MISC_FILE: list of additional drugs to search for
+    # 
+    # NB: files should have a line for each distinct drug type, 
+    #      and drugs should be separated by a vertical bar '|'
+    # 
+    # LIMIT FOR PARSING: max number of notes to search.
+    # OUTPUT: name of the output file.
+    """
+
+    if os.path.isfile(SUMMARY_FILE):
+        print('The output file already exists.\n\nRemove the following file or save with a different filename:')
+        print(os.path.join(os.getcwd(), SUMMARY_FILE))
+        return
+
     starttime = time.time()
     
     # Keep a list of all generics we are looking for
@@ -82,11 +86,15 @@ def main():
     # Get the drugs into a structure we can use
     with open(SSRI_FILE) as f:
         SSRI = readDrugs(f, genList)
-    with open(MISC_FILE) as f:
-        MISC = readDrugs(f, genList)
+        print("Using drugs from {}".format(SSRI_FILE))
+    try: 
+        with open(MISC_FILE) as f:
+            MISC = readDrugs(f, genList)
+            print("Using additional drugs from {}".format(MISC_FILE))
+    except:
+        MISC = None
     flatList = [item for sublist in genList for item in sublist]
 
-    
     # Create indices for the flat list
     # This allows us to understand which "types" are being used
     lengths = [len(type) for type in genList]
@@ -97,105 +105,89 @@ def main():
         starts.append(prevLeng)
         ends.append(prevLeng + leng - 1)
         prevLeng = prevLeng + leng
-    
-    # Get the list of filenames (unqualified)
-    filenames = filter(lambda x: os.path.isfile(os.path.join(NOTES_PATH, x)), 
-        os.listdir(NOTES_PATH))
-    filenames = filenames[:NUM_FILES_LIMIT]
+
+    # Limit the analysis to discharge summaries
+    # Comment out because limitation is now in SQL query
+    # NOTES = NOTES[NOTES['category'] == 'Discharge summary']
 
     # Write heads and notes to new doc
-    with open(SUMMARY_FILE, 'w') as f_out:
-        print >>f_out, "SUBJECT_ID|HIST_FOUND|DEPRESSION|ADMIT_FOUND|DIS_FOUND|GEN_DEPRESS_MEDS_FOUND|GROUP|SSRI|MISC|" + "|".join(flatList) + "\n"
+    with open(SUMMARY_FILE, 'a') as f_out:
+        f_out.write('"ROW_ID","SUBJECT_ID","HADM_ID","HIST_FOUND","DEPRESSION","ADMIT_FOUND","DIS_FOUND","GEN_DEPRESS_MEDS_FOUND","GROUP","SSRI","MISC","' \
+            + '","'.join(flatList) + '"\n')
 
         # Parse each patient record
-        print "Reading documents..."
-        for i, doc in enumerate(filenames):
-            if i % 100 == 0:
-                print "%d.. %s\n" % (i, doc),
+        print("Reading documents...")
+
+        for note in NOTES.itertuples():
+            if note.Index % 100 == 0:
+                print("...index: {}. row_id: {}. subject_id: {}. hadm_id: {}. \n".format(note.Index, note.row_id, note.subject_id, note.hadm_id))
                 sys.stdout.flush()
+            
+            # Reset some per-patient variables
+            section = ""
+            newSection = ""
+            admitFound = 0 # admission note found
+            dischargeFound = 0 # discharge summary found
+            histFound = 0 # medical history found
+            depressionHist = 0;
+            drugsAdmit = [0]*len(flatList)
+            drugsDis = [0]*len(flatList)
+            general_depression_drugs = 0
 
-            # Read heads and notes from doc
-            with open(os.path.join(NOTES_PATH, doc)) as f:
+            # Read through lines sequentially
+            # If this looks like a section header, start looking for drugs
+            for line in note.text.split("\n"): 
+
+                # Searches for a section header based on heuristics
+                m = re.search("""^((\d|[A-Z])(\.|\)))?\s*([a-zA-Z',\.\-\*\d\[\]\(\) ]+)(:| WERE | IS | ARE |INCLUDED|INCLUDING)""", line, re.I)
+                if m:
+                    newSection = ""
+                    # Past Medical History Section
+                    if re.search('med(ical)?\s+hist(ory)?', line, re.I):
+                        newSection = "hist"
+                        histFound = 1
+
+                    # Discharge Medication Section                                                        
+                    elif re.search('medication|meds', line, re.I) and re.search('disch(arge)?', line, re.I):
+                        newSection = "discharge"
+                        dischargeFound = 1
+
+                    # Admitting Medication Section
+                    elif re.search('admission|admitting|home|nh|nmeds|pre(\-|\s)?(hosp|op)|current|previous|outpatient|outpt|outside|^[^a-zA-Z]*med(ication)?(s)?', line, re.I) \
+                    and (section == "admit" or re.search('medication|meds', line, re.I)):
+                        newSection = "admit"
+                        admitFound = 1                                         
+                        
+                    # Med section ended, now in non-meds section                        
+                    if section != newSection:
+                        section = newSection
                 
-                # Read in the headers
-                nheads = f.readline().strip().split("_:-:_")
-                heads = nheads[:-1]
+                # If in history section, search for depression
+                if 'hist' in section:
+                    if re.search('depression', line, re.I):
+                        depressionHist = 1
 
-                # Create a regex to match notes from the headers
-                regex = "^" + "_:-:_".join("(?P<%s>.*?)" % (x) for x in heads) + "$"
-
-                # Read the notes
-                matches = re.finditer(regex, f.read(), re.MULTILINE | re.DOTALL)
-                notes = [m.groupdict() for m in matches]
-                
-                # Find the first discharge summary
-                for note in notes:
-                    if re.search('discharge.*summary', note['CATEGORY'], re.I):
-                        notes = note
-                        break
-                
-                # Reset some per-patient variables
-                section = ""
-                newSection = ""
-                admitFound = 0
-                dischargeFound = 0
-                histFound = 0
-                depressionHist = 0;
-                drugsAdmit = [0]*len(flatList)
-                drugsDis = [0]*len(flatList)
-                general_depression_drugs = 0
-                m = re.search('NOTE-EVENTS-([0-9]+).txt', doc, re.I)
-                sid = int(m.group(1))
-
-                # Read through lines sequentially
-                # If this looks like a section header, start looking for drugs
-                for line in note['TEXT'].split("\n"):    
-
-                    # Searches for a section header based on my heuristics
-                    m = re.search("""^((\d|[A-Z])(\.|\)))?\s*([a-zA-Z',\.\-\*\d\[\]\(\) ]+)(:| WERE | IS | ARE |INCLUDED|INCLUDING)""", line, re.I)
-                    if m:
-                        newSection = ""
-                        # Past Medical History Section
-                        if re.search('med(ical)?\s+hist(ory)?', line, re.I):
-                            newSection = "hist"
-                            histFound = 1
-
-                        # Discharge Medication Section                                                        
-                        elif re.search('medication|meds', line, re.I) and re.search('disch(arge)?', line, re.I):
-                            newSection = "discharge"
-                            dischargeFound = 1
-
-                        # Admitting Medication Section
-                        elif re.search('admission|admitting|home|nh|nmeds|pre(\-|\s)?(hosp|op)|current|previous|outpatient|outpt|outside|^[^a-zA-Z]*med(ication)?(s)?', line, re.I) and (section == "admit" or re.search('medication|meds', line, re.I)):
-                            newSection = "admit"
-                            admitFound = 1                                         
-                            
-                        # Med section ended, now in non-meds section                        
-                        if section != newSection:
-                            section = newSection
-                    
-                    # If in history section, search for depression
-                    if 'hist' in section:
-                        if re.search('depression', line, re.I):
-                            depressionHist = 1
-
-                    # If in meds section, look at each line for specific drugs
-                    elif 'admit' in section:
-                        drugsAdmit = addToDrugs(line, drugsAdmit, SSRI, flatList)
+                # If in meds section, look at each line for specific drugs
+                elif 'admit' in section:
+                    drugsAdmit = addToDrugs(line, drugsAdmit, SSRI, flatList)
+                    if MISC:
                         drugsAdmit = addToDrugs(line, drugsAdmit, MISC, flatList)
-                        
-                        ## Section just has something like 'Depression meds' 
-                        if re.search('depression\s+med(ication)?(s)?', line, re.I):
-                            general_depression_drugs = 1
-                        
-                    ## Already in meds section, look at each line for specific drugs
-                    elif 'discharge' in section:
-                        drugsDis = addToDrugs(line, drugsDis, SSRI, flatList)
+                    
+                    ## Section just has something like 'Depression meds' 
+                    if re.search('depression\s+med(ication)?(s)?', line, re.I):
+                        general_depression_drugs = 1
+                    
+                ## Already in meds section, look at each line for specific drugs
+                elif 'discharge' in section:
+                    drugsDis = addToDrugs(line, drugsDis, SSRI, flatList)
+                    if MISC:
                         drugsDis = addToDrugs(line, drugsDis, MISC, flatList)                        
-                        
-                    # A line with information which we are uncertain about... 
-                    elif re.search('medication|meds', line, re.I) and re.search('admission|discharge|transfer', line, re.I):
-                        print '?? ' + line
+                    
+                # A line with information which we are uncertain about... 
+                elif re.search('medication|meds', line, re.I) and re.search('admission|discharge|transfer', line, re.I):
+                    if VERBOSE:
+                        print('?? {}'.format(line))
+                    pass
 
             group = 0
             # Group 0: Patient has no medications on admission section (or no targeted meds) 
@@ -218,9 +210,12 @@ def main():
                 group = 3
                                 
             else:
-                print 'Uncertain about group type for ' + doc
+                if VERBOSE:
+                    print('Uncertain about group type for row_id = {}'.format(note.row_id))
+                pass
 
-            print 'group is ' + str(group) 
+            if VERBOSE:
+                print('group is {}'.format(group))
 
             # Combine the admit and discharge drugs lists
             combined = [w or x for w, x in zip(drugsAdmit, drugsDis)]
@@ -229,14 +224,14 @@ def main():
             member = []
             member = [int(1 in drugsAdmit[s:e+1]) for s, e in zip(starts, ends)]
 
-            # Print items for this patient into csv
-            print >>f_out, str(sid) + "|" + str(histFound) + "|" + str(depressionHist) + "|" + str(admitFound) + "|" + str(dischargeFound) + "|" + str(general_depression_drugs) + "|" + str(group) + "|" + "|".join(map(str, member)) + "|" + "|".join(map(str, drugsAdmit)) + "\n"
+            # save items to csv
+            f_out.write(str(note.row_id) + "," + str(note.subject_id) + "," + str(note.hadm_id) + "," + str(histFound) + "," \
+                + str(depressionHist) + "," + str(admitFound) + "," + str(dischargeFound) + "," \
+                + str(general_depression_drugs) + "," + str(group) + "," + ",".join(map(str, member)) \
+                + "," + ",".join(map(str, drugsAdmit)) + "\n")
 
-    # Print analysis
+    # Print summary of analysis
     stoptime = time.time()
-    print "Done analyzing %d documents in %.2f seconds (%.2f docs/sec)" % (i, stoptime - starttime, i / (stoptime - starttime))
-    print "Summary file is in %s" % (DRUGS_LIST_PATH)
-            
-if __name__ == "__main__":
-    main()
-
+    print("Done analyzing {} documents in {} seconds ({} docs/sec)".format(len(NOTES), 
+        round(stoptime - starttime, 2), round(len(NOTES) / (stoptime - starttime), 2)))
+    print("Summary file is in {}".format(os.getcwd()))
